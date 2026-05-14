51b80953de99b25b5128df1a08225bdf9597aebe
Jiahui Huang <jiahuih@nvidia.com>
2026-05-14 15:01:05 -0700

    Share Eigen setup with JIT extension builds

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 import os
-import shutil
-import tarfile
-import tempfile
-from urllib.request import urlretrieve
 
 from setuptools import find_packages, setup
 
@@ -34,29 +30,8 @@ if "CONDA_PREFIX" in os.environ:
     if os.path.exists(conda_nvcc_path):
         os.environ["PYTORCH_NVCC"] = conda_nvcc_path
 
-# Download the put Eigen 3.4 in a correct place
 cpp_flags = get_cpp_flags()
 cuda_flags = get_cuda_flags()
-if os.environ.get("USE_SYSTEM_EIGEN", "0") == "0":
-    eigen_include_dir = "csrc/include/eigen3"
-    eigen_url = "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz"
-
-    if not os.path.exists(eigen_include_dir):
-        os.makedirs(eigen_include_dir, exist_ok=True)
-
-        with tempfile.TemporaryDirectory() as temp_dir:
-            tmp_tar_path = os.path.join(temp_dir, "eigen.gz")
-            extracted_dir = os.path.join(temp_dir, "eigen-extracted")
-            urlretrieve(eigen_url, tmp_tar_path)
-            with tarfile.open(tmp_tar_path, "r:gz") as tar:
-                tar.extractall(path=extracted_dir)
-
-            shutil.move(os.path.join(extracted_dir, "eigen-3.4.0", "Eigen"), eigen_include_dir)
-
-    # Use full path
-    additional_include_path = os.path.join(os.path.dirname(__file__), "csrc/include")
-    cpp_flags += ["-isystem", additional_include_path]
-    cuda_flags += ["-isystem", additional_include_path]
 
 packages = find_packages()
 setup(
